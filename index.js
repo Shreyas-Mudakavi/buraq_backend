eeb12f4be27bfed78df845079d786dd3f32a7154
Shreyas Mudakavi <shreyasmudak@gmail.com>
2023-03-28 16:36:40 +0530

    first commit

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
 
+app.get("/", (req, res, next) =>
+  res.json({ status: "You have reached buraq backend!" })
+);
+
 app.use("/api/user", userRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/vehicle", vehicleRoutes);
